e80bf5d13264f1a8e317dbee11fabad304d09eba
HaHung2 <hahung0202@gmail.com>
2023-10-14 18:59:39 +0700

    first commit

--- client/shop.js
+++ client/shop.js
@@ -5,7 +5,9 @@ const inpAddProductName = document.querySelector('.inp-add-productName');
 const inpAddProductPrice = document.querySelector('.inp-add-productPrice');
 const inpAddProductImg = document.querySelector('.inp-add-productImg');
 const btnAddProduct = document.querySelector('.btn-add-product');
+const outPutProduct = document.querySelector('.out_put_productt');
 const apiUrl = 'http://localhost:4000/product';
+
 
 
 const addOneProduct = async ( productName, productPrice, productImg ) => {
@@ -31,11 +33,47 @@ const addOneProduct = async ( productName, productPrice, productImg ) => {
         // Xử lý kết quả ở đây, ví dụ: hiển thị sản phẩm mới được tạo ra
         console.log('Product created:', createdProduct);
         alert("Thêm product thành công")
+        getAllProduct();
       } catch (error) {
         // Xử lý lỗi ở đây
         console.error('Error:', error);
       }
 }
+
+
+
+const getAllProduct = async () => {
+  const response = await fetch(`${apiUrl}`);
+
+  if (response.status === 200) {
+    const data = await response.json();
+    console.log(47, "Lấy tất cả product thành công:", data);
+
+    const ListProduct = data?.map((value,index) => {
+      return
+      `
+          <div class="col-12 col-md-4 col-lg-3 mb-5" data-id="${value._id}">
+            <a class="product-item" href="#">
+              <img src="${value.productImg}"
+                class="img-fluid product-thumbnail">
+              <h3 class="product-title">${value.productName}</h3>
+              <strong class="product-price">${value.productPrice}</strong>
+
+              <span class="icon-cross">
+                <img src="images/cross.svg" class="img-fluid">
+              </span>
+            </a>
+          </div>
+        `;
+      })
+      outPutProduct.innerHTML = ListProduct.join(" ")
+  } else {
+    console.log("Lấy tất cả product thất bại:", response.status);
+  }
+};
+
+
+getAllProduct()
 
 
 btnOpenForm.addEventListener("click", () => {
